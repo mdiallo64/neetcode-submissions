9debc30670a8afaa64d54a5bc46f849ced88cb9e
mdiallo64 <142463226+mdiallo64@users.noreply.github.com>
2026-08-03 15:49:59 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(const std::string& s, const std::string& t)
+{
+    if (s.length() != t.length()) return false;
+
+    int count[26] {};
+    for (int i = 0; i < s.length(); ++i)
+    {
+        ++count[s[i] - 'a'];
+        --count[t[i] - 'a'];
+    }
+    for (int c : count)
+        if (c != 0) return false;
+    return true;
+}
+};
